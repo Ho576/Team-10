71d827ef49a40f033ffaf9956deafaf5ebf66c06
Ho576 <127043686+Ho576@users.noreply.github.com>
2023-12-10 09:01:21 +0200

    firebaseCreate

---
+++ src/components/routs.js
@@ -0,0 +1,29 @@
+// import { createBrowserRouter } from "react-router-dom";
+
+
+// export const router = createBrowserRouter([
+//     {
+//       path:'/',
+//       element:<Layout />,
+//       children:[
+//           {
+//             //path:'',
+//             index:true,
+//             element:<Home />
+//           },
+//           {
+//             path:'add',
+//             element:<Add />
+//           },
+//           {
+//             path:'edit',
+//             element:<Edit/>
+
+//           },
+//           {
+//             path:'*',
+//             element:<h2>page not found --- web</h2>
+//           }
+//       ]
+//     }
+//   ]);
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-export default function firebase() {
-  return (
-    <div>firebase</div>
-  )
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyDtf37MHho04ohqEQ_YAp9QO2K0hO-PBSQ",
+  authDomain: "admin-blog-c3848.firebaseapp.com",
+  projectId: "admin-blog-c3848",
+  storageBucket: "admin-blog-c3848.appspot.com",
+  messagingSenderId: "972628865096",
+  appId: "1:972628865096:web:9d8330a41ea2715fdae87d",
+  measurementId: "G-HSE7XX218H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app);
