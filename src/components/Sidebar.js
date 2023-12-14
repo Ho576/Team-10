483b74d93ba12c76e94b49c866cfadf487aba613
Ho576 <127043686+Ho576@users.noreply.github.com>
2023-12-14 13:50:34 +0200

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,129 +1,93 @@
-// import React from 'react'
-// import { Outlet } from 'react-router-dom'
+import React, { useState } from 'react'
+import './Sidebar.css'
+import { Link, Outlet } from 'react-router-dom'
 
-// export default function Sidebar() {
-//   return (
-//     <div className="container-fluid">
-//     <div className="row flex-nowrap">
-//       <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-//         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-//           <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-//             <span className="fs-5 d-none d-sm-inline">Menu</span>
-//           </a>
-//           <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-//             <li className="nav-item">
-//               <a href="#" className="nav-link align-middle px-0">
-//                 <i className="fs-4 bi-house" /> <span className="ms-1 d-none d-sm-inline">Home</span>
-//               </a>
-//             </li>
-//             <li>
-//               <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-//                 <i className="fs-4 bi-speedometer2" /> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-//               <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-//                 <li className="w-100">
-//                   <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1 </a>
-//                 </li>
-//                 <li>
-//                   <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2 </a>
-//                 </li>
-//               </ul>
-//             </li>
-//             <li>
-//               <a href="#" className="nav-link px-0 align-middle">
-//                 <i className="fs-4 bi-table" /> <span className="ms-1 d-none d-sm-inline">Orders</span></a>
-//             </li>
-//             <li>
-//               <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
-//                 <i className="fs-4 bi-bootstrap" /> <span className="ms-1 d-none d-sm-inline">Bootstrap</span></a>
-//               <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-//                 <li className="w-100">
-//                   <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1</a>
-//                 </li>
-//                 <li>
-//                   <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2</a>
-//                 </li>
-//               </ul>
-//             </li>
-//             <li>
-//               <a href="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-//                 <i className="fs-4 bi-grid" /> <span className="ms-1 d-none d-sm-inline">Products</span> </a>
-//               <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-//                 <li className="w-100">
-//                   <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
-//                 </li>
-//                 <li>
-//                   <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 2</a>
-//                 </li>
-//                 <li>
-//                   <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 3</a>
-//                 </li>
-//                 <li>
-//                   <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 4</a>
-//                 </li>
-//               </ul>
-//             </li>
-//             <li>
-//               <a href="#" className="nav-link px-0 align-middle">
-//                 <i className="fs-4 bi-people" /> <span className="ms-1 d-none d-sm-inline">Customers</span> </a>
-//             </li>
-//           </ul>
-//           <hr />
-//         </div>
-//       </div>
-//       <div className="col py-3">
-//         <Outlet/>
-//       </div>
-//     </div>
-//   </div>
-//   )
-// }
+export default function Sidebar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
-const Sidebar = () => {
   return (
-    <div className="container-fluid">
-      <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-            <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-              <span className="fs-5 d-none d-sm-inline">Menu</span>
+    <div id="layoutSidenav" className={`mySidebar ${isSidebarOpen ? 'open' : 'closed'}`} >
+    <div id="layoutSidenav_nav">
+      <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div className="sb-sidenav-menu">
+          <div className="nav">
+            <div className="sb-sidenav-menu-heading">Core</div>
+            <Link className="nav-link" to={'/'}>
+              <div className="sb-nav-link-icon"><i className="bi bi-speedometer2 text-white" /></div>
+              Dashboard
+            </Link>
+            <div className="sb-sidenav-menu-heading">Interface</div>
+            <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+              <div className="sb-nav-link-icon"><i className="bi bi-layout-split text-wihte" /></div>
+              Layouts
+              <div className="sb-sidenav-collapse-arrow"><i className="bi bi-chevron-down text-white" /></div>
             </a>
-            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-              <li className="nav-item">
-                <a href="#" className="nav-link align-middle px-0">
-                  <i className="fs-4 bi-house" /> <span className="ms-1 d-none d-sm-inline">Home</span>
-                </a>
-              </li>
-              <li>
-                <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                  <i className="fs-4 bi-speedometer2" /> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-                <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                  <li className="w-100">
-                    <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1 </a>
-                  </li>
-                  <li>
-                    <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2 </a>
-                  </li>
-                </ul>
-              </li>
-              {/* ... (rest of your navigation links) */}
-            </ul>
-            <hr />
-            <div className="sb-sidenav-footer">
-              <div className="small">Logged in as:</div>
-              Start Bootstrap
+            <div className="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+              <nav className="sb-sidenav-menu-nested nav">
+                <a className="nav-link" href="#">Static Navigation</a>
+                <a className="nav-link" href="#">Light Sidenav</a>
+              </nav>
             </div>
+            <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+              <div className="sb-nav-link-icon"><i className="bi bi-book-fill text-white" /></div>
+              Pages
+              <div className="sb-sidenav-collapse-arrow"><i className="bi bi-chevron-down text-white" /></div>
+            </a>
+            <div className="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+              <nav className="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                  Authentication
+                  <div className="sb-sidenav-collapse-arrow"><i className="bi bi-chevron-down text-white" /></div>
+                </a>
+                <div className="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                  <nav className="sb-sidenav-menu-nested nav">
+                    <a className="nav-link" href="#">Login</a>
+                    <a className="nav-link" href="#">Register</a>
+                    <a className="nav-link" href="#">Forgot Password</a>
+                  </nav>
+                </div>
+                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                  Error
+                  <div className="sb-sidenav-collapse-arrow"><i className="bi bi-chevron-down text-white" /></div>
+                </a>
+                <div className="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                  <nav className="sb-sidenav-menu-nested nav">
+                    <a className="nav-link" href="#">401 Page</a>
+                    <a className="nav-link" href="#">404 Page</a>
+                    <a className="nav-link" href="#">500 Page</a>
+                  </nav>
+                </div>
+              </nav>
+            </div>
+            <div className="sb-sidenav-menu-heading">Addons</div>
+            <a className="nav-link" href="#">
+              <div className="sb-nav-link-icon"><i className="bi bi-graph-up text-white" /></div>
+              Charts
+            </a>
+            <a className="nav-link" href="#">
+              <div className="sb-nav-link-icon"><i className="bi bi-table text-white" /></div>
+              Tables
+            </a>
           </div>
         </div>
-        <div className="col py-3">
-          <Outlet />
-          {/* You can replace the Outlet component with the component where you want to display the main content. */}
+        <div className="sb-sidenav-footer">
+          <div className="small">Logged in as:</div>
+          Start Bootstrap
         </div>
-      </div>
+      </nav>
     </div>
-  );
-};
+    <div className='contentt'>
+        <Outlet/>
+        <p>footer</p>
+    </div>
+    
+    
+  </div>
 
-export default Sidebar;
+
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+ import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
